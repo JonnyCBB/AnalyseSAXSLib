07f23c5d516f2eaeb36b2be75a4748186553a2b1
JonnyCBB <jonathan.brooks-bartlett@dtc.ox.ac.uk>
2016-02-25 15:13:14 +0000

    add method to plot Correlation Map

--- CorMapAnalysis.py
+++ CorMapAnalysis.py
@@ -5,13 +5,24 @@ import glob
 import subprocess
 import re
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 
 class ScatterAnalysis(object):
     """CorMap class implementing analysis of data output from a run of the
     ATSAS suite program DATCMP
     """
+    # ----------------------------------------------------------------------- #
+    #                         CLASS VARIABLES                                 #
+    # ----------------------------------------------------------------------- #
+    PLOT_LABEL = {'family': 'serif',
+                  'weight': 'normal',
+                  'size': 16}
+    mpl.rc('font', family='serif', weight='normal', size=12)
+
+    PLOT_NUM = 0
+
     # ----------------------------------------------------------------------- #
     #                         CONSTRUCTOR METHOD                              #
     # ----------------------------------------------------------------------- #
@@ -46,6 +57,56 @@ class ScatterAnalysis(object):
                                                                float(data[4]),
                                                                float(data[5])]
         return data_dict
+
+    def calc_cormap(self):
+        """Return CorMap matrix i.e. the (Pearson product-moment) correlation
+        matrix.
+        """
+        return np.corrcoef(self.I)
+
+    def plot_cormap(self, colour_scheme="gray", display=True, save=False,
+                    filename="", directory=""):
+        """Create a plot object of a CorMap
+        """
+        # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+        #                          SET PLOT PARAMS                        #
+        # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+        min_q = min(self.q)
+        max_q = max(self.q)
+        self.PLOT_NUM += 1
+
+        # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+        #                          PLOT CORMAP                            #
+        # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+        plt.figure(self.PLOT_NUM)
+        cormap = plt.imshow(self.calc_cormap(), cmap=colour_scheme,
+                            extent=[min_q, max_q, min_q, max_q])
+        plt.xlabel(r'Scattering Vector, q (nm$^{-1}$)',
+                   fontdict=self.PLOT_LABEL)
+        plt.ylabel(r'Scattering Vector, q (nm$^{-1}$)',
+                   fontdict=self.PLOT_LABEL)
+        plt.colorbar(cormap)
+
+        # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+        #                       SAVE AND/OR DISPLAY                       #
+        # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+        if save and filename:
+            if directory:
+                plot_path = "{}/{}".format(directory, filename)
+            else:
+                plot_path = filename
+            plt.savefig(plot_path)
+        elif save and not filename:
+            print "********************** ERROR ***************************"
+            print "COULD NOT SAVE PLOT"
+            print "No filename specified. Please specify a filename if you"
+            print "would like to save the plot."
+        if display:
+            plt.show()
+
+# --------------------------------------------------------------------------- #
+#                               FUNCTIONS                                     #
+# --------------------------------------------------------------------------- #
 
 
 def run_system_command(command_string):
