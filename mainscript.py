6160507a2cc02b64f0234a00c5176f7c03e69bad
JonnyCBB <jonathan.brooks-bartlett@dtc.ox.ac.uk>
2016-02-28 19:32:11 +0000

    update the "mainscript.py" file. Now it runs according to the changes in the other modules

--- mainscript.py
+++ mainscript.py
@@ -1,5 +1,20 @@
 """Main script to analyse radiation damage in the SAXS data
 """
-from scattercurveutils import plot_1d_curve_with_filename
-FILE_NAME = "../20151214/GI_Gly/1d/GI_Gly_111_00001.dat"
-plot_1d_curve_with_filename(FILE_NAME, 1, 1200, True)
+# import numpy as np
+# from CorMapAnalysis import ScatterAnalysis
+from RadioProtectant import Compound
+# from scattercurveutils import plot_1d_curve_with_filename
+# FILE_NAME = "../20151214/GI_Gly/1d/GI_Gly_111_00001.dat"
+# plot_1d_curve_with_filename(FILE_NAME, 1, 1300, True)
+
+a = Compound(compound_name="np", crop_start=25, crop_end=700, overwrite=True)
+b = Compound(compound_name="asc", crop_start=25, crop_end=700, overwrite=True)
+# files = "../cropped/GI_no_protection/1d/GI_np_030*.dat"
+# files = "../20151214/GI_no_protection/1d/GI_np_034*.dat"
+# files = "../cropped/GI_Asc/1d/GI_Asc_096*.dat"
+# dose = np.linspace(1,120,120)/2.0
+# metric = "dose"
+# units = "kGy"
+# a = ScatterAnalysis(files)
+# b = ScatterAnalysis(files, dose, metric, units)
+# a.plot_1d_intensity(1)
