3f064750365c5c6f3c8a1885eedf609da06d4708
JonnyCBB <jonathan.brooks-bartlett@dtc.ox.ac.uk>
2016-03-06 03:28:54 +0000

    add all compounds to the analysis

--- mainscript.py
+++ mainscript.py
@@ -4,8 +4,8 @@ from CompareRadioProtectants import ComparisonAnalysis
 
 P_thresholds = [0.01, 0.05, 0.10]
 num_consec_frames = [1, 3, 5, 7, 10]
-# compounds = ["np", "asc", "dtt", "suc", "tempo", "tre", "no3", "gly", "etgly"]
-compounds = ["np", "asc"]
+compounds = ["np", "asc", "dtt", "suc", "tempo", "tre", "no3", "gly", "etgly"]
+# compounds = ["np", "asc"]
 a = ComparisonAnalysis(compound_list=compounds, crop_start=25, crop_end=700,
                        overwrite=False, overwrite_doses=False,
                        num_consec_frames=num_consec_frames,
